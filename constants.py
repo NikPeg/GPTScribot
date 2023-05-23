a188b9f804ba33c4b2df982bc9ba2c4e04bc718c
NikPeg <peganov.nik@gmail.com>
2023-05-23 18:49:57 +0300

    Delete $ and &

--- constants.py
+++ constants.py
@@ -1,6 +1,9 @@
 USELESS_SYMBOLS = {'-', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ' '}
 SECTION = "\\section"
-SPECIAL_SYMBOLS = {"$", "&"}
+SPECIAL_SYMBOLS = {"$", "&", "_"}
+USELESS_SEQUENCES = {"url"}
 BIBLIOGRAPHIES = {"Библиография", "Список использованных источников", "Источники", "Материалы"}
 NEW_LINE = "\\\\\n"
+NEW_PAGE = "\\newpage"
+END_DOCUMENT = "\n\\end{document}"
 NAME_USELESS_SYMBOLS = {'.', '"', "'"}
