7f5ddd1f8c5c9f5b1de39045117133e3fa21a785
NikPeg <peganov.nik@gmail.com>
2023-05-23 23:53:39 +0300

    Add % in constants

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
-USELESS_SYMBOLS = {'-', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ' '}
+USELESS_SYMBOLS = {'-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ' '}
 SECTION = "\\section"
-SPECIAL_SYMBOLS = {"$", "&", "_"}
+SPECIAL_SYMBOLS = {"$", "&", "_", "%"}
 USELESS_SEQUENCES = {"url"}
 BIBLIOGRAPHIES = {"Библиография", "Список использованных источников", "Источники", "Материалы"}
 NEW_LINE = "\\\\\n"
