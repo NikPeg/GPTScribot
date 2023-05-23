ba753c95aa02133289db75153e7bd36bac0b7633
NikPeg <peganov.nik@gmail.com>
2023-05-23 20:40:25 +0300

    Bug with text

--- models.py
+++ models.py
@@ -87,9 +87,9 @@ class CourseWorkFactory:
     def _replace_special_symbols(text):
         res = text
         for c in SPECIAL_SYMBOLS:
-            text.replace(c, f"\\{c}")
+            res.replace(c, f"\\{c}")
         for seq in USELESS_SEQUENCES:
-            text.replace(seq, "")
+            res.replace(seq, "")
         return res
 
     def _validate_chapter(self, text, name):
