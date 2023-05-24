2bc7c3781606576362e8deaa49d586c01e8e3257
NikPeg <peganov.nik@gmail.com>
2023-05-24 14:00:30 +0300

    Add blank lines processing

--- models.py
+++ models.py
@@ -137,7 +137,7 @@ class CourseWorkFactory:
     def _chapter_with_blank_lines(text):
         res = ""
         for line in text.split("\n"):
-            if line:
+            if line.strip():
                 res += f"{line}\n" if line.endswith("}") or "\\item" in line else f"{line}{BLANK_LINE}"
         return res
 
