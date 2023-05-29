3c0556e5ff246e79026c0757aeb808046e4d7c1d
NikPeg <peganov.nik@gmail.com>
2023-05-30 01:01:11 +0300

    Add dollar replacing

--- constants.py
+++ constants.py
@@ -2,7 +2,6 @@ USELESS_SYMBOLS = {'-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '
 SECTION = "\\section"
 BIBLIOGRAPHY_SECTION = "\\section*"
 SPECIAL_SYMBOLS = {"%"}
-# SPECIAL_SYMBOLS = {"$", "%"}
 BIBLIOGRAPHY_SPECIAL_SYMBOLS = {"$", "&", "_", "%"}
 USELESS_SEQUENCES = {"\\url", "[label={[\\arabic*]}]"}
 BIBLIOGRAPHIES = {"Библиография", "Список использованных источников", "Источники", "Материалы", "Список литературы"}
