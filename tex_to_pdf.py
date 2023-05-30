75085fe8b2cccb29267384648e804ed2294f0f71
Nikita Peganov <peganov.nik@gmail.com>
2023-05-30 10:54:47 +0300

    Normal way to make pdf files

--- tex_to_pdf.py
+++ tex_to_pdf.py
@@ -1,4 +1,4 @@
-from pylatex import Document, Section, Subsection, Command
+from pylatex import Document, Section, Subsection, Command, NoEscape
 
 doc = Document()
 section = Section('Hello')
@@ -6,4 +6,9 @@ subsection = Subsection('World')
 doc.append(section)
 section.append(subsection)
 subsection.append('Hello World!')
-doc.generate_pdf()
+next_command = "\\newpage"
+raw_s = rf'{next_command}'
+doc.append(NoEscape(raw_s))
+doc.append(Section("Hehe"))
+doc.append(Section("Hehe"))
+doc.generate_pdf('full', clean_tex=False)
