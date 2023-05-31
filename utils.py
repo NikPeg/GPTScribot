a73285d830615aeeec4e45ad99918115b5e205d0
Nikita Peganov <peganov.nik@gmail.com>
2023-05-31 17:09:26 +0300

    Normal logging

--- utils.py
+++ utils.py
@@ -1,10 +1,17 @@
 from config import DEBUG, ADMIN
 
 
+MAX_MESSAGE_LENGTH = 4096
+
+
 def log(text, bot=None):
-    if not text:
-        return
-    if DEBUG:
-        print(text)
-    if bot:
-        bot.send_message(ADMIN, text)
+    try:
+        if not text:
+            return
+        if DEBUG:
+            print(text)
+        if bot:
+            for i in range(0, len(text), MAX_MESSAGE_LENGTH)
+                bot.send_message(ADMIN, text[i:i + MAX_MESSAGE_LENGTH])
+    except Exception:
+        pass
