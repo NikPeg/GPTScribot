dd4365d707cbaad7f539cf1b922d59111e6104b8
NikPeg <peganov.nik@gmail.com>
2024-01-03 17:58:59 +0300

    Not always log

--- work_generator.py
+++ work_generator.py
@@ -168,10 +168,10 @@ class CourseWorkFactory:
     def _ask_to_replace(self, match):
         symbol = match.re.pattern
         s = match.string
-        log(f"Len of string: {len(s)}, symbol index: {match.span()[0]}", self.bot)
         if match.span()[0] == len(s):
             return ""
         log(f"Asking GPT about symbol {symbol}", self.bot)
+        log(f"Len of string: {len(s)}, symbol index: {match.span()[0]}", self.bot)
         substring = s[max(0, match.span()[0] - 50):min(len(s), match.span()[0] + 50)]
         log(f"Substring to ask: {substring}", self.bot)
         gpt_answer = self.gpt.ask(SYMBOLS_TO_ASK[symbol][0].format(substring))
