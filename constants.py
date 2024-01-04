299bedc3ce29360788f1712b77c9f640756e6dc4
NikPeg <peganov.nik@gmail.com>
2024-01-05 00:41:39 +0300

    Section*

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ from gpt_messages import AMPERSAND_QUESTION, DOLLAR_QUESTION
 
 USELESS_SYMBOLS = {'-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ' '}
 SECTION = "\\section"
-BIBLIOGRAPHY_SECTION = "\\section*"
+BIBLIOGRAPHY_SECTION = "\\section"
 SPECIAL_SYMBOLS = {"%"}
 BIBLIOGRAPHY_SPECIAL_SYMBOLS = {"$", "&", "_", "%"}
 USELESS_SEQUENCES = {"\\url", "[label={[\\arabic*]}]"}
