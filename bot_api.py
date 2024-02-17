16673ecf989617a14b62ab12a186d9b08144c43e
NikPeg <peganov.nik@gmail.com>
2024-02-18 00:13:27 +0300

    check message changing

--- bot_api.py
+++ bot_api.py
@@ -1,6 +1,10 @@
+from telebot.apihelper import ApiTelegramException
+
 from messages import *
 from constants import *
 from telebot import types
+
+from utils import log
 
 
 def edit_status_message(message, bot, ready_chapters=0, chapters_count=10):
@@ -10,10 +14,16 @@ def edit_status_message(message, bot, ready_chapters=0, chapters_count=10):
     btn1 = types.InlineKeyboardButton(text='🏠Главное меню', callback_data='menu')
     markup.add(btn1)
     ready_count = round(ready_chapters / chapters_count * 10)
-    bot.edit_message_text(
-        STATUS_MESSAGE.format(ready_count * READY_SYMBOL + UNREADY_SYMBOL * (10 - ready_count)),
-        reply_markup=markup,
-        parse_mode='Markdown',
-        chat_id=message.chat.id,
-        message_id=message.message_id
-    )
+    text = message.text
+    if text.count(READY_SYMBOL) == ready_count:
+        return
+    try:
+        bot.edit_message_text(
+            STATUS_MESSAGE.format(ready_count * READY_SYMBOL + UNREADY_SYMBOL * (10 - ready_count)),
+            reply_markup=markup,
+            parse_mode='Markdown',
+            chat_id=message.chat.id,
+            message_id=message.message_id
+        )
+    except Exception as e:
+        log(e, bot)
