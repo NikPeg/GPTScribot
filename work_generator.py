952252b975482f5699e4d82fb3f3334e27d3f86c
NikPeg <peganov.nik@gmail.com>
2024-04-15 00:17:50 +0300

    work_type.value in file

--- work_generator.py
+++ work_generator.py
@@ -141,7 +141,7 @@ class CourseWork:
             with io.open(f"template{i}.tex", mode="r", encoding="utf-8") as template:
                 res += template.read()
             if i < 3:
-                res += f"{self.upper_name}{BIG_INDENT}{self.work_type}{NEW_LINE}"
+                res += f"{self.upper_name}{BIG_INDENT}{self.work_type.value}{NEW_LINE}"
         with io.open(f"templateFree.tex", mode="r", encoding="utf-8") as template:
             free_text = template.read().format(price=config.PRICE)
         if free:
